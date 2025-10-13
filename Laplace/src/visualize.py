e02beb75e5a1099a69e18b260caa43da4c24383f
Reksi M. Silaban <reksisilaban76@gmail.com>
2025-10-13 20:21:24 +0700

    Delete Laplace directory

--- Laplace/src/visualize.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt 
-
-def plot_heatmap(phi):
-    plt.imshow(phi, origin='lower', cmap='hot')
-    plt.colorbar(label='Tempetature')
-    plt.title('Laplace Equation Solution (2D)')
-    plt.show()
